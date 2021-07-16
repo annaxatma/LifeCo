9a195cf7feb3addd95b1e487adba963555d48e76
Tinara Nathania <50069062+tinaranw@users.noreply.github.com>
2021-07-16 11:04:26 +0700

    summmm codes

--- lifeco/app/src/main/java/com/example/LifeCo/activities/AddGroupActivity.java
+++ lifeco/app/src/main/java/com/example/LifeCo/activities/AddGroupActivity.java
@@ -1,12 +1,25 @@
 package com.example.LifeCo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.lifeco.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AddGroupActivity extends AppCompatActivity {
+
+//    private static final int
+    Toolbar groupChatToolbar;
+
+    //Firebase
+    FirebaseAuth firbaseAuth;
+
+    ImageView groupChatIcon;
+    TextInputLayout groupNameLayout, groupDescLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
