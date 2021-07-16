449a97e71451f8a240b6244a68508eaaca55cabc
NickySan <nickysantano123@gmail.com>
2021-07-16 10:46:29 +0700

    for group

---
+++ lifeco/app/src/main/java/com/example/LifeCo/activities/GroupChatActivity.java
@@ -0,0 +1,123 @@
+package com.example.LifeCo.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.LifeCo.Adapter.UserAdapter;
+import com.example.LifeCo.model.Chat;
+import com.example.LifeCo.model.Users;
+import com.example.lifeco.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    private UserAdapter userAdapter;
+    private List<Users> mUsers;
+
+    FirebaseUser fuser;
+    DatabaseReference reference;
+
+    private List<String> usersList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+
+//        View view = inflater.inflate(R.layout.activity_group_chat, container, false);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(GroupChatActivity.this));
+
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        usersList = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usersList.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Chat chat = snapshot.getValue(Chat.class);
+
+                    if(chat.getSender().equals(fuser.getUid())){
+                        usersList.add(chat.getReceiver());
+                    }
+                    if (chat.getReceiver().equals(fuser.getUid())){
+                        usersList.add(chat.getSender());
+                    }
+                }
+
+                readChats();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void readChats() {
+        mUsers = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsers.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Users users = snapshot.getValue(Users.class);
+
+                    //display 1 user from chats
+                    for (String id : usersList){
+                        if (users.getId().equals(id)){
+                            if (mUsers.size() != 0){
+                                for (Users users1 : mUsers){
+                                    if (!users.getId().equals(users1.getId())){
+                                        mUsers.add(users);
+                                    }
+                                }
+                            }else{
+                                mUsers.add(users);
+                            }
+                        }
+                    }
+                }
+
+                userAdapter = new UserAdapter(GroupChatActivity.this, mUsers, true);
+                recyclerView.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+}
