2aaa458226cc5505019a23ff0aae28b7f576a26e
NickySan <nickysantano123@gmail.com>
2021-07-21 10:51:21 +0700

    layout profile

--- lifeco/app/src/main/java/com/example/LifeCo/fragments/SOCSProfileFragment.java
+++ lifeco/app/src/main/java/com/example/LifeCo/fragments/SOCSProfileFragment.java
@@ -11,56 +11,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.LifeCo.activities.SplashScreenNew;
 import com.example.lifeco.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SOCSProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SOCSProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     Button btnLogOut;
+    TextView nama, email;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+
     public SOCSProfileFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SOCSProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SOCSProfileFragment newInstance(String param1, String param2) {
-        SOCSProfileFragment fragment = new SOCSProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -74,7 +44,25 @@ public class SOCSProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        nama = view.findViewById(R.id.txtNamaDriver);
+        email = view.findViewById(R.id.txtEmailDriver);
         btnLogOut = view.findViewById(R.id.btnLogOut);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+//        DocumentReference documentReference = fStore.collection("Users").document(userId);
+//        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@javax.annotation.Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+//                nama.setText(documentSnapshot.getString("name"));
+//                email.setText(documentSnapshot.getString("email"));
+//            }
+//        });
+
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
